f6c3fe7a8ec30cc277e5685942c0e18331a600c0
jmuthialu <jmuthialu@yahoo.com>
2015-04-19 18:47:38 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppPg::Application.config.secret_token = 'ecc7fbba9ca7ecc9e5ddc976055eb007746b4b0726b2f4c50751674858a9eb4ad878eff4c29e1f10d36a5c2eaf5551b78bb92f3c6d63a51945b932d597f5218d'
